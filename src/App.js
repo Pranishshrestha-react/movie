24ff73f592bc5116f09dc0b2bacfa4c894585cc9
Pranishshrestha-react <respectsweetleaf@gmail.com>
2021-02-07 02:31:32 +0545

    container edited

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ import './App.css';
 
 function App() {
   return(
+    <div className="body">
   <BrowserRouter>
     <NavBar />
-    <div class="container-fluid">
+
     
       
     <Switch>
@@ -21,9 +22,10 @@ function App() {
 
 
     </Switch>
-    </div>
+  
   
   </BrowserRouter>
+  </div>
   );
 }
 
