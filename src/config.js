453685282a9eb4e8ccf312133daf4e8235a2010b
Pranishshrestha-react <respectsweetleaf@gmail.com>
2021-02-07 02:31:55 +0545

    image placed original

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_KEY = 'e147b49fad62daaf91427891b4bc7904';
 export const BASE_URL = 'https://api.themoviedb.org/3';
-export const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/w500';
+export const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/original';
