d7c2ff77a915d26e3846e1a068488ed3b691bd2c
Pranishshrestha-react <respectsweetleaf@gmail.com>
2021-02-06 13:13:48 +0545

    genre made simple

--- src/components/dropdown/genre.js
+++ src/components/dropdown/genre.js
@@ -1,20 +1,8 @@
-import React, { useState} from 'react';
-import axios from 'axios';
-
-  import { BASE_URL, API_KEY} from '../../config.js';
-  import DropDown from './index.js';
+import React  from 'react';
+import DropDown from './index.js';
 
 const Genre = () => {
-    const [genreSelect, setGenreSelect] = React.useState([]);
-
-    React.useEffect(() => {
-        getGenreSelect();
-    }, []);
-
-    const getGenreSelect = async() => {
-        const res = await axios.get(`${BASE_URL}/genre/movie/list`, {params: { api_key: API_KEY}});
-        setGenreSelect(res.data.result);
-    }
+    
     return(
         <div>
             <DropDown />
