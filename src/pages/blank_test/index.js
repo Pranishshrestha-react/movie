1ffb12e52a87d395e785c5b0acd8c6b9a0af490e
Pranishshrestha-react <respectsweetleaf@gmail.com>
2021-02-08 16:09:24 +0545

    blank created to check

---
+++ src/pages/blank_test/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {BASE_URL , API_KEY} from '../../config.js';
+import axios from 'axios';
+import Genre from '../../components/dropdown';
+
+const ArrayGen= () => {
+    const [arrayGen, setArrayGen] = React.useState([]);
+
+    React.useEffect(() =>{
+        getArrayGen();
+    }, []);
+    
+    const getArrayGen = async() => {
+        const res = await axios.get(`${BASE_URL}/genre/movie/list` , {params:{ api_key: API_KEY}});
+        setArrayGen(res.data.genres);
+        
+
+    }
+    
+    return(
+    <div style={{marginTop:'4.5%'}}>
+        <div>
+            {
+                arrayGen.map(listG => {
+                    return <Genre listG={listG}/>;
+                })
+            }
+        </div> 
+    </div>
+    )
+}
+export default ArrayGen;
